564d06dea68a1a03418c260d027ae4b9443b12de
Hailong Yang <hailongyang@HailongdeMacBook-Pro.local>
2026-03-01 22:25:44 +0900

    chore: 添加开启 Supabase Realtime 的 SQL 脚本

---
+++ supabase/enable_realtime.sql
@@ -0,0 +1,6 @@
+-- 开启游戏相关表的数据变更实时广播
+-- 注意：在 Supabase 中，只有加入到了 supabase_realtime publication 里的表才会把数据更改发送到 WebSocket
+ALTER PUBLICATION supabase_realtime ADD TABLE games;
+ALTER PUBLICATION supabase_realtime ADD TABLE game_players;
+ALTER PUBLICATION supabase_realtime ADD TABLE buy_ins;
+ALTER PUBLICATION supabase_realtime ADD TABLE pending_buyins;
